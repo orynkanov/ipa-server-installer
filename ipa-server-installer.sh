03d25eac9205808baf4d190fdcee97d24948acf7
orynkanov <orynkanov@gmail.com>
2020-07-08 23:08:03 +0600

    fix

--- ipa-server-installer.sh
+++ ipa-server-installer.sh
@@ -2,8 +2,9 @@
 
 IPETH0=$(ip addr show eth0 | grep 'inet ' | awk '{print $2}' | cut -d '/' -f1)
 echo "$IPETH0 $(hostname -f) $(hostname -s)" >> /etc/hosts
+REVZONE=$(echo $IPETH0 | awk -F. '{print $3"." $2"."$1}')'.in-addr.arpa.'
 
-ipa-server-install -p '1q2w#E$R' -a '1q2w#E$R' -n 'yo.virt' -r 'YO.VIRT' --hostname="$(hostname -f)" --setup-dns --mkhomedir --no-dns-sshfp --forwarder=8.8.8.8 --forwarder=8.8.4.4
+ipa-server-install -p '1q2w#E$R' -a '1q2w#E$R' -n 'yo.virt' -r 'YO.VIRT' --hostname="$(hostname -f)" --setup-dns --reverse-zone="$REVZONE" --mkhomedir --no-dns-sshfp --forwarder='8.8.8.8' --forwarder='8.8.4.4'
 
 #   Basic options:
 #     -p DM_PASSWORD, --ds-password=DM_PASSWORD
@@ -36,3 +37,30 @@ ipa-server-install -p '1q2w#E$R' -a '1q2w#E$R' -n 'yo.virt' -r 'YO.VIRT' --hostn
 #   DNS options:
 #     --forwarder=FORWARDERS
 #                         Add a DNS forwarder. This option can be used multiple
+
+
+
+
+
+
+
+  DNS options:
+    --allow-zone-overlap
+                        Create DNS zone even if it already exists
+    --reverse-zone=REVERSE_ZONE
+                        The reverse DNS zone to use. This option can be used
+                        multiple times
+    --no-reverse        Do not create new reverse DNS zone
+    --auto-reverse      Create necessary reverse zones
+    --zonemgr=ZONEMGR   DNS zone manager e-mail address. Defaults to
+                        hostmaster@DOMAIN
+    --forwarder=FORWARDERS
+                        Add a DNS forwarder. This option can be used multiple
+                        times
+    --no-forwarders     Do not add any DNS forwarders, use root servers
+                        instead
+    --auto-forwarders   Use DNS forwarders configured in /etc/resolv.conf
+    --forward-policy={only,first}
+                        DNS forwarding policy for global forwarders
+    --no-dnssec-validation
+                        Disable DNSSEC validation
