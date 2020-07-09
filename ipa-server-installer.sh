f5dbdf7eae3363ef7fb2b0a1a1618539afe02a93
orynkanov <orynkanov@gmail.com>
2020-07-09 21:42:00 +0600

    fix firewall

--- ipa-server-installer.sh
+++ ipa-server-installer.sh
@@ -14,7 +14,17 @@ source "$SCRIPTDIR"/vars.cfg
 
 IPETH0=$(ip addr show eth0 | grep 'inet ' | awk '{print $2}' | awk -F/ '{print $1}')
 echo "$IPETH0 $(hostname -f) $(hostname -s)" >> /etc/hosts
-REVZONE=$(echo "$IPETH0" | awk -F. '{print $3"." $2"."$1}')'.in-addr.arpa.'
 
+firewall-cmd --add-service=freeipa-4
+firewall-cmd --permanent --add-service=freeipa-4
+
+firewall-cmd --add-service=dns
+firewall-cmd --permanent --add-service=dns
+
+firewall-cmd --add-service=ntp
+firewall-cmd --permanent --add-service=ntp
+
+#REVZONE=$(echo "$IPETH0" | awk -F. '{print $3"." $2"."$1}')'.in-addr.arpa.'
 #ipa-server-install -p "$DMPASS" -a "$ADMINPASS" -n "$DOMAIN" -r "${REALM^^}" --hostname="$(hostname -f)" --setup-dns --reverse-zone="$REVZONE" --mkhomedir -N --no-dns-sshfp --forwarder="$DNSFORWARDER1" --forwarder="$DNSFORWARDER2" -U
+
 ipa-server-install -p "$DMPASS" -a "$ADMINPASS" -n "$DOMAIN" -r "${REALM^^}" --hostname="$(hostname -f)" --setup-dns --auto-reverse --mkhomedir -N --no-dns-sshfp --forwarder="$DNSFORWARDER1" --forwarder="$DNSFORWARDER2" -U
